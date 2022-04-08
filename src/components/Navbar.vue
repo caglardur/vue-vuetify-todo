47d42c74f996c40745ac179ee9355d5f7bf3fb54
caglar.durmus <caglardur@gmail.com>
2022-04-08 22:53:43 +0300

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <v-app-bar flat app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title class="text-uppercase">
+        <span class="font-weight-light">TODO</span>
+        <span class="font-weight-bold">Vuetify</span>
+      </v-toolbar-title>
+      <v-spacer />
+      <v-btn flat color="white" depressed>
+        <span>Sign Out</span>
+        <v-icon right>mdi-exit-to-app</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" app class="purple lighten-4">
+      <v-list>
+        <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+          <v-list-item-icon>
+            <v-icon class="white--text" v-text="link.icon"></v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title class="white--text" v-text="link.text"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Navbar-component",
+  data() {
+    return {
+      drawer: false,
+      links: [
+        { icon: "mdi-view-dashboard", text: "Dashboard", route: "/" },
+        { icon: "mdi-folder", text: "My Projects", route: "/projects" },
+        { icon: "mdi-account", text: "Team", route: "/team" }
+      ]
+    }
+  }
+}
+</script>
+
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h1>Home Page</h1>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Vue from "vue"
 
-@Component({
-  components: {
-    HelloWorld,
-  },
+export default Vue.extend({
+  name: "Home-View",
+
+  components: {}
 })
-export default class HomeView extends Vue {}
 </script>
